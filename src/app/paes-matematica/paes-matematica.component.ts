8a1dd89670058864d45804e256776d4f215561e2
Alexis Tripailao <atripailao@amisoft.ai>
2024-08-20 08:23:55 -0400

    primera version de evaluación de respuestas

--- src/app/paes-matematica/paes-matematica.component.ts
+++ src/app/paes-matematica/paes-matematica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RespuestasMatematica} from './respuestas-matematica';
 
 @Component({
   selector: 'app-paes-matematica',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaesMatematicaComponent implements OnInit {
 
+  respuestasCorrectas: RespuestasMatematica[] = [
+    { numeroPregunta: 1, respuesta: 'A' },
+    { numeroPregunta: 2, respuesta: 'C' },
+    { numeroPregunta: 3, respuesta: 'C' },
+    { numeroPregunta: 4, respuesta: 'D' }
+  ];
+
+  respuestasUsuario: RespuestasMatematica[] = [
+    { numeroPregunta: 1, respuesta: 'A' },
+    { numeroPregunta: 2, respuesta: 'C' },
+    { numeroPregunta: 3, respuesta: 'C' },
+    { numeroPregunta: 4, respuesta: 'D' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  comprobarRespuestas() {
+    this.respuestasUsuario.forEach((respuestaUsuario, index) =>{
+      const respuestaCorrecta = this.respuestasCorrectas[index];
+      if (respuestaUsuario.respuesta === respuestaCorrecta.respuesta){
+        console.log(`Repuesta ${respuestaUsuario.numeroPregunta} correcta`);
+      } else {
+        console.log(`Respuesta ${respuestaUsuario.numeroPregunta} incorrecta`);
+      }
+    });
   }
 
 }
---
+++ src/app/paes-matematica/respuestas-matematica.ts
@@ -0,0 +1,4 @@
+export interface RespuestasMatematica {
+  numeroPregunta: number;
+  respuesta: string;
+}
